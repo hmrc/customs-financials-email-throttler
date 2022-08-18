4b786f38ff0214c9fda79abfa125186491d9c424
muhammadibraaheem <46523489+muhammadibraaheem@users.noreply.github.com>
2022-08-18 12:03:46 +0100

    upgrade mongo-play library

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"        % "0.56.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"        % "0.70.0",
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28" % "5.16.0"
   )
 
