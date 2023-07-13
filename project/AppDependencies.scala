7c597d2033da07f1184a6ce09c2bebed198aef71
tyagikumarvineet <tyagi.kumar.vineet@gmail.com>
2023-07-13 14:08:04 +0100

    [MMCA-4102] Refactor add val for bootstrap version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,16 +2,18 @@ import sbt.*
 
 object AppDependencies {
 
+  val bootstrapVersion = "7.19.0"
+
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"        % "1.3.0",
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28" % "7.19.0"
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0",
+    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapVersion
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "7.19.0"                % Test,
-    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.0.0"                 % "test",
-    "org.mockito"             %  "mockito-core"             % "3.1.0"                 % "test"
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapVersion % Test,
+    "org.pegdown" % "pegdown" % "1.6.0" % "test",
+    "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % "test",
+    "org.mockito" % "mockito-core" % "3.1.0" % "test"
   )
 
 }
